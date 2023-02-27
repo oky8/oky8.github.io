06b78f153e3c9f86c0a50a9a5ee022165c14e069
okkyefand <okyhirakazuto8@gmail.com>
2023-02-27 21:25:38 +0700

    perubahan tombol top dan perbaikan menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixednav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixednav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -18,3 +23,11 @@ humberger.addEventListener("click", function () {
   humberger.classList.toggle("humberger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// klik diluar humberger untuk menghilangkan menu
+window.addEventListener("click", function (e) {
+  if (e.target != humberger && e.target != navMenu) {
+    humberger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
+})
