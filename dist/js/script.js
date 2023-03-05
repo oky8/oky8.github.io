cd35abe36568eca046d4aa1ffc6994de6b519b1d
okkyefand <okyhirakazuto8@gmail.com>
2023-03-05 16:40:21 +0700

    final darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -41,7 +41,20 @@ const html = document.querySelector('html');
 darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
     html.classList.add('dark');
+    localStorage.theme = 'dark';
   }else{
     html.classList.remove('dark');
+    localStorage.theme = 'light'; 
   }
 })
+
+// pindahkan toggle sesuai data localstorage
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
